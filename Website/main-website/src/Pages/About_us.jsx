01c966a75da53802b7e99a803866bb3bfb300e13
Padmanabh Patadiya <pvpcreations05@gmail.com>
2025-01-03 11:52:05 +0530

    3-1-25 (2)

---
+++ Website/main-website/src/Pages/About_us.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const About_us = () => {
+  return (
+    <>
+    
+    <div className="main_head_div">
+      <div className="heading">
+        About Us
+        <div className="navigations"><a href="/">Home</a> | About Us</div>
+      </div>
+      <div className="description">
+      Enhance your skills with our comprehensive IT training programs designed for professionals and individuals
+      </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default About_us
---
+++ Website/main-website/src/Pages/Contact_us.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Contact_us = () => {
+  return (
+    <>
+    
+    <div className="main_head_div">
+      <div className="heading">
+        Contact Us
+        <div className="navigations"><a href="/">Home</a> | Contact Us</div>
+      </div>
+      <div className="description">
+      Enhance your skills with our comprehensive IT training programs designed for professionals and individuals
+      </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Contact_us
